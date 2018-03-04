6ca243acaab28a0bfea6f7adf12a966bd31a93d6
kong <441610411@qq.com>
2018-03-04 18:15:22 +0800

    创建

---
+++ src/wuGong/duGuJiuJian.java
@@ -0,0 +1,10 @@
+package wuGong;
+
+public class duGuJiuJian {
+
+	public static void main(String[] args) {
+		System.out.println("11111111111111");
+		System.out.println("22222222222222");
+	}
+
+}
