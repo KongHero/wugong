da6a9f103a03ff9c52235259d9532ee1a1cd8835
kong <441610411@qq.com>
2018-03-04 19:27:51 +0800

    添加

--- src/wuGong/duGuJiuJian.java
+++ src/wuGong/duGuJiuJian.java
@@ -7,6 +7,9 @@ public class duGuJiuJian {
 		System.out.println("22222222222222");
 		System.out.println("33333333333333");
 		System.out.println("44444444444444");
+		System.out.println("55555555555555555");
+		System.out.println("6666666666666");
+		System.out.println("4777777777777777");
 	}
 
 }
